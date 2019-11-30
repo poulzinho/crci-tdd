4bd8178d7f17214eadd00d1231f8035f05e5f2d6
Paul Gualotuna <paul.gualotuna.dev@gmail.com>
2019-11-30 18:12:31 +0100

    feat(chapter01): palindrome permutation, return true for the palindrome taco cat

--- src/ch-01_arrays_and_strings/1.4_palindrome-permutation/palindrome-permutation.spec.ts
+++ src/ch-01_arrays_and_strings/1.4_palindrome-permutation/palindrome-permutation.spec.ts
@@ -1,5 +1,13 @@
+import {isPalindromePerm} from "./palindrome-permutation";
+
 describe('canary spec for palindrome permutation', () => {
     it('shows the infrastructure works', () => {
         expect(true).toBeTruthy();
     })
+});
+
+describe('a palindrome permutation function', () => {
+    it('should return true for a palindrome such as "taco cat"', () => {
+        expect(isPalindromePerm('taco cat')).toBeTruthy();
+    });
 });
---
+++ src/ch-01_arrays_and_strings/1.4_palindrome-permutation/palindrome-permutation.ts
@@ -0,0 +1,3 @@
+export const isPalindromePerm = (aString: string) => {
+    return aString === 'taco cat'
+};
