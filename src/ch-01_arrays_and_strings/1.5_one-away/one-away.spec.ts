5bd1210225e2752e3c7c2a5c501169761812676e
Paul Gualotuna <paul.gualotuna.dev@gmail.com>
2019-12-01 20:12:12 +0100

    feat(chapter01): one away, removed character II

--- src/ch-01_arrays_and_strings/1.5_one-away/one-away.spec.ts
+++ src/ch-01_arrays_and_strings/1.5_one-away/one-away.spec.ts
@@ -8,8 +8,10 @@ describe('canary spec for one away', () => {
 
 describe('a one away function', () => {
     it('should check if a string is one removed character away from another string. ' +
-        'For example, this is true for "pale" and "ple".', () => {
+        'For example, this is true for "pale" and "ple". ' +
+        'Same for "pale" and "pal".', () => {
 
-        expect(oneAway('pale', 'ple')).toBeTruthy;
+        expect(oneAway('pale', 'ple')).toBeTruthy();
+        expect(oneAway('pale', 'pal')).toBeTruthy();
     });
 });
--- src/ch-01_arrays_and_strings/1.5_one-away/one-away.ts
+++ src/ch-01_arrays_and_strings/1.5_one-away/one-away.ts
@@ -1,3 +1,15 @@
-export const oneAway = (aString: string, bString: string) => {
-    return aString === 'pale' && bString === 'ple';
+export const oneAway = (aString: string, bString: string): boolean => {
+
+    const aArray = aString.split('');
+    const bArray = bString.split('');
+
+    let countAway = 0;
+
+    aArray.forEach((bChar) => {
+        if (!bArray.includes(bChar)) {
+            countAway++;
+        }
+    });
+
+    return countAway === 1;
 };
