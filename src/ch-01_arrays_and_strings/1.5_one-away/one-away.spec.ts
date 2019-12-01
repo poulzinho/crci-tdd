90e6460bd544e1ba9a618f5270f98f25b01f2617
Paul Gualotuna <paul.gualotuna.dev@gmail.com>
2019-12-01 21:18:33 +0100

    feat(chapter01): one away, replaced character
    refactored considering pointers instead of characters included in array

--- src/ch-01_arrays_and_strings/1.5_one-away/one-away.spec.ts
+++ src/ch-01_arrays_and_strings/1.5_one-away/one-away.spec.ts
@@ -13,10 +13,19 @@ describe('a one away function', () => {
 
         expect(oneAway('pale', 'ple')).toBeTruthy();
         expect(oneAway('pale', 'pal')).toBeTruthy();
+        expect(oneAway('pales', 'pale')).toBeTruthy();
+        expect(oneAway('paleso', 'pale')).toBeFalsy();
     });
 
     it('should check if a string is one added character from another string', () => {
-        expect(oneAway('pale', 'pales')).toBeTruthy();
+        expect(oneAway('ple', 'pale')).toBeTruthy();
         expect(oneAway('pal', 'pale')).toBeTruthy();
+        expect(oneAway('pale', 'pales')).toBeTruthy();
+        expect(oneAway('pale', 'paleso')).toBeFalsy();
+    });
+
+    it('should check if a string is one replaced character from another string', () => {
+        expect(oneAway('pale', 'bale')).toBeTruthy();
+        expect(oneAway('pale', 'bake')).toBeFalsy();
     });
 });
--- src/ch-01_arrays_and_strings/1.5_one-away/one-away.ts
+++ src/ch-01_arrays_and_strings/1.5_one-away/one-away.ts
@@ -2,18 +2,33 @@ export const oneAway = (aString: string, bString: string): boolean => {
 
     const aArray = aString.split('');
     const bArray = bString.split('');
+    const lengthAMinusB = aArray.length - bArray.length;
 
-    let countAway = 0;
+    let countOneAway = 0;
 
-    if (aArray.length >= bArray.length) {
-        aArray.forEach((bChar) => {
-            if (!bArray.includes(bChar)) countAway++;
-        });
-    } else {
-        bArray.forEach((bChar) => {
-            if (!aArray.includes(bChar)) countAway++;
-        });
+    if (lengthAMinusB === 1) {
+        for (let i = 0, j = 0; i < aArray.length; i++) {
+            if (aArray[i] !== bArray[j]) {
+                countOneAway++;
+                j--;
+            }
+            j++;
+        }
+    } else if (lengthAMinusB === -1) {
+        for (let i = 0, j = 0; i < bArray.length; i++) {
+            if (bArray[i] !== aArray[j]) {
+                countOneAway++;
+                j--;
+            }
+            j++;
+        }
+    } else if (lengthAMinusB === 0) {
+        for (let i = 0; i < aArray.length; i++) {
+            if (bArray[i] !== aArray[i]) {
+                countOneAway++;
+            }
+        }
     }
 
-    return countAway === 1;
+    return countOneAway === 1;
 };
