ded693570d2b64992abcbdd1471a10ad4cf659ae
Paul Gualotuna <paul.gualotuna.dev@gmail.com>
2019-12-07 11:49:58 +0100

    refactor(ch01 - rotate-matrix): use a temporal array

--- src/ch-01_arrays_and_strings/1.7_rotate-matrix/rotate-matrix.ts
+++ src/ch-01_arrays_and_strings/1.7_rotate-matrix/rotate-matrix.ts
@@ -1,29 +1,17 @@
 export const rotateMatrix = (matrix: unknown[][]): unknown[][] => {
-    const tmp = matrix.reduce((acum, curr) => {
-        curr.forEach(y => acum.push(y));
-        return acum;
-    }, []);
+
+    const result = [];
 
     for (let i = 0; i < matrix.length; i++) {
         for (let j = 0; j < matrix[i].length; j++) {
 
-            if (i === 0 && j === 0) {
-                matrix[i][j] = tmp[2]
+            if (!result[j]) {
+                result[j] = [];
             }
 
-            if (i === 0 && j === 1) {
-                matrix[i][j] = tmp[0]
-            }
-
-            if (i === 1 && j === 0) {
-                matrix[i][j] = tmp[3]
-            }
-
-            if (i === 1 && j === 1) {
-                matrix[i][j] = tmp[1]
-            }
+            result[j].unshift(matrix[i][j]);
         }
     }
 
-    return matrix;
+    return result;
 };
